34ab7a29f998f06dbe35d09719a365504f7fd175
VA <96442443+DIYPLC@users.noreply.github.com>
2022-10-02 19:12:57 +0300

    Add files via upload

---
+++ BLACK_PILL_401/Core/Src/main.c
@@ -0,0 +1,473 @@
+/* USER CODE BEGIN Header */
+/*
+ DIY PLC
+ STM32F401CCU6
+ CPU ARM Cortex-M4 32Bit 84MHz
+ RAM 64Kb
+ Flash 256Kb
+
+ UART1 9600Bod 8N1 MODBUS ASCII SLAVE ADR1 (черновик)
+
+ C Проект
+ STM32CubeIDE Version: 1.6.1
+
+ FREERTOS использует TIM1.
+ Стек по умолчанию 128слов = 512байт. слово = 4 байта
+ Всего стек TOTAL_HEAP_SIZE = 15360 байта
+ Code style = GNU
+ */
+
+// @COPYLEFT ALL WRONGS RESERVED :)
+// Author: VA
+// Contacts: DIY.PLC.314@gmail.com
+// License: GNU GPL v2
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdint.h>
+#include <stdbool.h>
+#include <iso646.h>
+#include "GlobalVar.h"
+#include "MODBUS.h"
+struct GlobalVar GV =
+  { 0 };
+struct DbMODBUS DbMODBUS =
+  { 0 };
+//#define TX_OFF() HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET)
+//#define TX_ON() HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET)
+#define LED_BUILTIN_ON() HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET)
+#define LED_BUILTIN_OFF() HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET)
+#define KEY_BUILTIN_STATE() (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == GPIO_PIN_RESET)
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
+
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes =
+  { .name = "defaultTask", .stack_size = 128 * 4, .priority =
+      (osPriority_t) osPriorityNormal, };
+/* Definitions for myTask02 */
+osThreadId_t myTask02Handle;
+const osThreadAttr_t myTask02_attributes =
+  { .name = "myTask02", .stack_size = 128 * 4, .priority =
+      (osPriority_t) osPriorityNormal, };
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void
+SystemClock_Config (void);
+static void
+MX_GPIO_Init (void);
+static void
+MX_USART1_UART_Init (void);
+void
+StartDefaultTask (void *argument);
+void
+StartTask02 (void *argument);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+uint8_t
+RxByte () //
+{
+  uint8_t Buff[2];
+  HAL_UART_Receive (&huart1, Buff, 1, 0xFFFF); //block fun
+  return Buff[0];
+}
+
+void
+RxMessage (struct DbMODBUS *p) //
+{
+  const uint8_t START_BYTE = 0x3A;
+  const uint8_t STOP_BYTE1 = 0x0D;
+  const uint8_t STOP_BYTE2 = 0x0A;
+  static uint8_t ByteCounter = 0;
+  uint8_t ReciveByte;
+  ReciveByte = RxByte ();
+//�?шем стартовый байт в <i> сброса и приема пакета.
+  if ((ReciveByte == START_BYTE)
+      && ((p->State == MODBUS_STATE_RESET) || (p->State == MODBUS_STATE_RX_ADU)))
+    {
+      p->State = MODBUS_STATE_RX_ADU;
+      ByteCounter = 0;
+      p->Message[0] = ReciveByte;
+    }
+  //recive bytes
+  while (p->State == MODBUS_STATE_RX_ADU)
+    {
+      ReciveByte = RxByte ();
+      ByteCounter = ByteCounter + 1;
+      //check OWF
+      if (ByteCounter <= 30)
+	{
+	  p->Message[ByteCounter] = ReciveByte; //OK
+	}
+      else
+	{
+	  p->State = MODBUS_STATE_RESET; //OWF
+	}
+      //find end bytes
+      if ((ReciveByte == STOP_BYTE2)
+	  && (p->Message[ByteCounter - 1] == STOP_BYTE1))
+	{
+	  p->State = MODBUS_STATE_ADU_TO_PDU;
+	  p->SizeMessage = ByteCounter + 1;
+	  ByteCounter = 0;
+	}
+    }
+  return;
+}
+
+void
+TxMessage (struct DbMODBUS *p) //
+{
+  //TX_ON();
+  //LED_ON();
+  HAL_UART_Transmit (&huart1, p->Message, p->SizeMessage, 0xFFFF);
+  p->State = MODBUS_STATE_RESET;
+  //TX_OFF();
+  //LED_OFF();
+  return;
+}
+/* USER CODE END 0 */
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int
+main (void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init ();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config ();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init ();
+  MX_USART1_UART_Init ();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Init scheduler */
+  osKernelInitialize ();
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew (StartDefaultTask, NULL,
+				   &defaultTask_attributes);
+
+  /* creation of myTask02 */
+  myTask02Handle = osThreadNew (StartTask02, NULL, &myTask02_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart ();
+
+  /* We should never get here as control is now taken by the scheduler */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+    {
+      /* USER CODE END WHILE */
+
+      /* USER CODE BEGIN 3 */
+    }
+  /* USER CODE END 3 */
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void
+SystemClock_Config (void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct =
+    { 0 };
+  RCC_ClkInitTypeDef RCC_ClkInitStruct =
+    { 0 };
+
+  /** Configure the main internal regulator output voltage
+   */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+  /** Initializes the RCC Oscillators according to the specified parameters
+   * in the RCC_OscInitTypeDef structure.
+   */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 84;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig (&RCC_OscInitStruct) != HAL_OK)
+    {
+      Error_Handler ();
+    }
+  /** Initializes the CPU, AHB and APB buses clocks
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+      | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig (&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+    {
+      Error_Handler ();
+    }
+}
+
+/**
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void
+MX_USART1_UART_Init (void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init (&huart1) != HAL_OK)
+    {
+      Error_Handler ();
+    }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void
+MX_GPIO_Init (void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct =
+    { 0 };
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin (LED_PC13_GPIO_Port, LED_PC13_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : LED_PC13_Pin */
+  GPIO_InitStruct.Pin = LED_PC13_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init (LED_PC13_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : SW1_PA0_Pin */
+  GPIO_InitStruct.Pin = SW1_PA0_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init (SW1_PA0_GPIO_Port, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+ * @brief  Function implementing the defaultTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartDefaultTask */
+void
+StartDefaultTask (void *argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  while (1)
+    {
+      osDelay (1); //Delay for MODBUS ASCII
+      GV.MW[0] = (uint16_t) (HAL_GetTick () / 1000);
+      if (KEY_BUILTIN_STATE())
+	{
+	  LED_BUILTIN_ON();
+	}
+      else
+	{
+	  LED_BUILTIN_OFF();
+	}
+      /* USER CODE END 5 */
+    }
+}
+/* USER CODE BEGIN Header_StartTask02 */
+/**
+ * @brief Function implementing the myTask02 thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartTask02 */
+void
+StartTask02 (void *argument)
+{
+  /* USER CODE BEGIN StartTask02 */
+  /* Infinite loop */
+  while (1)
+    {
+      //osDelay(1);
+      //MODBUS ASCII;
+      RxMessage (&DbMODBUS);
+      Fb_MODBUS_ASCII_SLAVE_ADU_TO_PDU (&DbMODBUS);
+      //taskENTER_CRITICAL();
+      Fb_MODBUS_SLAVE_PROCESSING_PDU (&DbMODBUS, &GV);
+      //taskEXIT_CRITICAL();
+      Fb_MODBUS_ASCII_SLAVE_PDU_TO_ADU (&DbMODBUS);
+      TxMessage (&DbMODBUS);
+    }
+  /* USER CODE END StartTask02 */
+}
+
+/**
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM1 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
+void
+HAL_TIM_PeriodElapsedCallback (TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM1)
+    {
+      HAL_IncTick ();
+    }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void
+Error_Handler (void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq ();
+  while (1)
+    {
+    }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
